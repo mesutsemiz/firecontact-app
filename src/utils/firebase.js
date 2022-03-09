47491be5eb25af38b3ec50c27de70cc0b7b21fa9
Mesut <81357924+mesutsemiz@users.noreply.github.com>
2022-03-09 22:25:14 +0300

    .env file created

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,16 +1,20 @@
 import firebase from "firebase/app";
 import "firebase/database"
 
-const firebaseConfig = {
+const devConfig = {
 
-    apiKey: "AIzaSyDJKCBXqPd7W-6ItIrFYl-2AihNvDDFgZ0",
-    authDomain: "firecontact-in-class-780de.firebaseapp.com",
-    projectId: "firecontact-in-class-780de",
-    storageBucket: "firecontact-in-class-780de.appspot.com",
-    messagingSenderId: "896191734087",
-    appId: "1:896191734087:web:d555c80077536bdd7dac62"
+    apiKey: process.env.REACT_APP_APİ_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAİN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_DATABASE_URL,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID
   
   };
+
+  const prodConfig= {};
+
+  const firebaseConfig = process.env.NODE_ENV === "development"? devConfig : prodConfig;
   
   firebase.initializeApp(firebaseConfig);
   export default firebase
